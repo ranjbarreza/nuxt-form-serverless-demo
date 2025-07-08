2a570b4db905ae995747452fd4c2e4fbf7c3b174
ranjbarreza <reza.r.baar@gmail.com>
2025-07-08 14:47:09 +0200

    enable nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui'],
+  css: ['~/assets/css/main.css']
 })
