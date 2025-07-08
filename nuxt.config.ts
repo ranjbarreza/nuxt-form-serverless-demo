5dfd7521ad839d90ff73a46f482ff496d68560a9
ranjbarreza <reza.r.baar@gmail.com>
2025-07-08 15:04:57 +0200

    add eslint and antfu eslint config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
 })
